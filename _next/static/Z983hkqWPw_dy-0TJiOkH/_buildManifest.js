de1474590174aec4f675340718ad5802ec12a108
J.sky <bosichong@qq.com>
2023-08-23 19:38:39 +0800

    更新

--- _next/static/Z983hkqWPw_dy-0TJiOkH/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/498-a269d9d2180ba714.js","static/chunks/pages/index-c7eedc7b2e3084aa.js"],"/_error":["static/chunks/pages/_error-d46847fd4080f9bd.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
